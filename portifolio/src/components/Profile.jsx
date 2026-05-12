10d41b5937228f30b23b2ba19d7eb01da40db562
kwuraa <kwuraa@gmail.com>
2026-05-12 00:17:41 -0300

    feat: create "Profile" component

---
+++ portifolio/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import react from 'react'
+import "../styles/Profile.css"
+
+const Profile = () => {
+    <container calssName="profile-window">
+        <img src="" alt="" />
+        <div className="terminal-buttons">
+            <span className="btn-close"></span>
+            <span className="btn-minimize"></span>
+            <span className="btn-maximize"></span>
+        </div>
+        <img src="" alt="" className="profile-photo" />
+        <div className="profile-social">
+            <a href="#" className="btn-social">INSTA</a>
+            <a href="#" className="btn-social">GitHub</a>
+            <a href="#" className="btn-social">E-mail</a>
+        </div>
+    </container>
+}
