4c8075eb3b2f8e7195e3e83477bbc2a2c5a109d2
kwuraa <kwuraa@gmail.com>
2026-05-28 12:29:20 -0300

    bug: fix

--- portifolio/src/components/Profile.jsx
+++ portifolio/src/components/Profile.jsx
@@ -1,17 +1,16 @@
 import react from 'react'
-import "../styles/Profile.css"
+import '../styles/Profile.css'
 import foto from '../assets/profile.png'
 
 const Profile = () => {
 return (
-    <container calssName="profile-window">
-        <img src={foto} alt="" />
+    <container className="profile-window">
         <div className="terminal-buttons">
             <span className="btn-close"></span>
             <span className="btn-minimize"></span>
             <span className="btn-maximize"></span>
         </div>
-        <img src="{foto}" alt="" className="profile-photo" />
+        <img src={foto} alt="" className="profile-photo" />
         <div className="profile-social">
             <a href="#" className="btn-social">INSTA</a>
             <a href="#" className="btn-social">GitHub</a>
