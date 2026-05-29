cda26c41be2898e05b622264eb972414c64d05e0
kwuraa <kwuraa@gmail.com>
2026-05-29 11:26:17 -0300

    feat: Edit profile window styles and hide navbar scroll animation

--- portifolio/src/components/Navbar.jsx
+++ portifolio/src/components/Navbar.jsx
@@ -1,16 +1,49 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import "../styles/Navbar.css"
+import { FaArrowDown } from 'react-icons/fa';
 
-const Navbar = () => {
+export default function Navbar() {
+  const [isHidden, setIsHidden] = useState(false);
+  const [isManuallyOpen, setIsManuallyOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () =>{
+    if(window.scrollY > 50) {
+        setIsManuallyOpen(false)
+        setIsHidden(true)
+    } else {
+      setIsHidden(false)
+      setIsManuallyOpen(false)
+    }
+  }
+  window.addEventListener('scroll', handleScroll);
+
+  return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handlePeekNavbar = () => {
+    setIsHidden(false);        
+    setIsManuallyOpen(true);   
+  };
+
   return (
-    <nav className="nav-container">
-      <div className="nav-pill">
+    <>
+    <nav>
+      <div className={`nav-pill ${isHidden ? 'hidden' : ''} ${isManuallyOpen ? 'open' : ''}`}>
         <a href="#home" className="nav-link">Home</a>
         <a href="#projects" className="nav-link">Projects</a>
         <a href="#contact" className="nav-link color-highlight">Contact Me</a>
       </div>
     </nav>
+
+    <button 
+        className={`nav-toggle-btn ${isHidden ? 'visible' : ''}`}
+        onClick={handlePeekNavbar}
+        aria-label="Mostrar menu"
+      >
+        <FaArrowDown className='nav-icon' size={38}/>
+      </button>
+    </>
   );
 };
-
-export default Navbar;
